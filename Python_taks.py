f50f3041a2582cc31aff8e94a58aab914b77dcb0
Ilias <iliasnuruev@gmail.com>
2022-08-01 15:39:09 +0700

    statement of work

---
+++ Python_taks.py
@@ -0,0 +1,92 @@
+"""1. На языке Python реализовать алгоритм (функцию) определения четности целого числа, который будет аналогичен нижеприведенному по функциональности, но отличен по своей сути. Объяснить плюсы и минусы обеих реализаций."""
+
+def is_even(value):
+    return value%1==0
+#плюсами данного кода является, то что функция может дать остаток от деления. Скорость работы при этом достаточно медленнее.
+
+"""2. На языке Python (2.7) реализовать минимум по 2 класса реализовывающих циклический буфер FIFO. Объяснить плюсы и минусы каждой реализации."""
+#1
+class RingBuffer:
+    def __init__(self,size_max):
+        self.max = size_max
+        self.data = []
+
+    class __Full:
+        def append(self, x):
+            self.data[self.cur] = x
+            self.cur = (self.cur+1) % self.max
+        def get(self):
+            return self.data[self.cur:]+self.data[:self.cur]
+
+    def append(self,x):
+        self.data.append(x)
+        if len(self.data) == self.max:
+            self.cur = 0
+            self.__class__ = self.__Full
+
+    def get(self):
+        return self.data
+
+
+#2
+class CircularBuffer(object):
+    def __init__(self, size):
+        self.index= 0
+        self.size= size
+        self._data = []
+
+    def record(self, value):
+        if len(self._data) == self.size:
+            self._data[self.index]= value
+        else:
+            self._data.append(value)
+        self.index= (self.index + 1) % self.size
+
+    def __getitem__(self, key):
+        return(self._data[key])
+
+    def __repr__(self):
+        return self._data.__repr__() + ' (' + str(len(self._data))+' items)'
+
+    def get_all(self):
+        return(self._data)
+
+"""3. На языке Python реализовать функцию, которая быстрее всего (по процессорным тикам) отсортирует данный ей массив чисел.
+Массив может быть любого размера со случайным порядком чисел (в том числе и отсортированным). Объяснить почему вы считаете, что функция соответствует заданным критериям."""
+
+def mergeSort(arr):
+    if len(arr) > 1:
+        mid = len(arr)//2
+
+        L = arr[:mid]
+
+        R = arr[mid:]
+
+        mergeSort(L)
+        mergeSort(R)
+
+        i = j = k = 0
+
+        while i < len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i += 1
+            else:
+                arr[k] = R[j]
+                j += 1
+            k += 1
+
+        while i < len(L):
+            arr[k] = L[i]
+            i += 1
+            k += 1
+
+         while j < len(R):
+             arr[k] = R[j]
+             j += 1
+             k += 1
+
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
